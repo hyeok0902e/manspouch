4eeb67688c8a8c7ee4bb244ed802e3e598242227
정연진 <softcorn@yeonjin-Macbook.local>
2018-12-20 13:17:23 +0900

    점심 먹기 전 + 프리티 드롭다운

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,8 +1,20 @@
 $(function () {
+
+    $(".signin").keydown(function(e) {
+        if (e.keyCode == 13){
+            e.preventDefault();
+            return false;
+        }
+    });
+
     $('.field_s1').fadeIn(2000);
 
     $('.sign_email').keyup(function (e) { 
+        sign_c1 = 0;
         var email11 = $(this).val();
+        var signvali =  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+
         $.ajax({url: '/check_email',method: 'POST',
         data: {email : email11},
         success: function(response){
@@ -11,15 +23,23 @@ $(function () {
                 $('.field_s2').slideUp(500);
                 $('.fs1_error').slideDown(500);
             }
+            else if(!signvali.test(email11)){
+                $('.sign_email').css("border-bottom","2px solid #D550A2");
+                $('.fs12_error').slideDown(500);
+            }
             else{
                 $('.sign_email').css("border-bottom","2px solid #875AED");
-                $('.field_s2').slideDown(500);
                 $('.fs1_error').slideUp(500);
+                $('.fs12_error').slideUp(500);
+                $('.field_s2').slideDown(500);
+                sign_c1 = 1;
             }
         }});
+
     });
 
     $('.sign_pwd1').keyup(function (e) { 
+        sign_c2 = 0;
         e.preventDefault();
 
         var pwd1 = $('.sign_pwd1').val();
@@ -30,6 +50,7 @@ $(function () {
         else{
             $('.sign_pwd1').css("border-bottom","2px solid #875AED");
             $('.field_s3').slideDown(500);
+            sign_c2 = 1;
         }
 
         pwd1 = "";
@@ -37,6 +58,7 @@ $(function () {
     });
 
     $('.sign_pwd2').keyup(function (e) { 
+        sign_c3 = 0;
         e.preventDefault();
 
         var pwd1 = $('.sign_pwd1').val();
@@ -45,37 +67,76 @@ $(function () {
         var tf = Boolean(pwd1===pwd2);
         if(tf){
             $('.sign_pwd2').css("border-bottom","2px solid #875AED");
-            $('.sign_next').slideDown(500);
+            $('.fs3_error').slideUp(500);
+            sign_c3 = 1;
         }
         else{
             $('.sign_pwd2').css("border-bottom","2px solid #D550A2");
-            $('.sign_next').slideUp(500);
+            $('.fs3_error').slideDown(500);
         }
 
         pwd1 = "";
         pwd2 = "";
     });
 
+    $(document).keyup(function(e){
+        sign_r1 = sign_c1+sign_c2+sign_c3;
+        if(sign_r1 == 3){
+            $('.sign_next').slideDown(500);
+        }
+        else{
+            $('.sign_next').slideUp(500);
+        }
+    });
+    
     $('.sign_next').click(function (e) { 
         e.preventDefault();
         
         $('.signup_1').slideUp(500);    
         $('.field').delay(500).fadeIn(500);    
-        $('.signin_text').empty().append("<span>회원 가입</span><br>1단계 - 2단계 : 추가 회원정보 - 3단계" );
+        $('.signin_text').empty().append("<span>회원 가입</span><br>1단계 - <b>2단계 : 추가 회원정보</b> - 3단계" );
+        $('.prettydropdown').css('display','inline-block');
+    });
+
+    $('.sign_nick').keyup(function (e) { 
+        sign_c4 = 0;
+
+        var signchk = $('.sign_nick').val();
+        var signvali =  /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]+$/;
+        
+        if(!signvali.test(signchk)){
+            $('.sign_nick').css("border-bottom","2px solid #D550A2");
+            $('.fs4_error').slideDown(500);
+        }
+        else if(signchk.length < 3 || signchk.length > 15){
+            $('.sign_nick').css("border-bottom","2px solid #D550A2");
+            $('.fs41_error').slideDown(500);
+        }
+        else{
+            $('.sign_nick').css("border-bottom","2px solid #875AED");
+            $('.fs41_error').slideUp(500);
+            $('.fs4_error').slideUp(500);
+            sign_c4 = 0;
+        }
     });
 
     $('.sign_tel').keyup(function (e) { 
-        e.preventDefault();
+        sign_c5 = 0;
 
         var tel = $('.sign_tel').val();
-        var telnum =  /(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/g;
+        var telnum = /^\d{8,15}$/;
         
         if(telnum.test(tel)){
             $('.sign_tel').css("border-bottom","2px solid #875AED");
+            $('.fs5_error').slideUp(500);
+            sign_c5 = 1;
         }
         else{
             $('.sign_tel').css("border-bottom","2px solid #D550A2");
+            $('.fs5_error').slideDown(500);
         }
     });
+
+    $('.sign_select').prettyDropdown();
     
 });
