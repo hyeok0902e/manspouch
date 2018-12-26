0e41eeddfea5708f400d3aca45bde404333077e9
Minseok Bang <minseokbang@Minseokui-MacBookAir.local>
2018-12-26 12:09:26 +0900

    product admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,4 +7,21 @@ class AdminController < ApplicationController
 		@creatures = Creature.all
 
 	end
+
+	def userinfo
+		@users = User.all
+	end
+
+	def productinfo
+		@products = Product.all
+	end
+
+	def contentinfo
+		@contents = Content.all
+	end
+
+	def creatureinfo
+		@creatures = Creature.all
+	end
+
 end
