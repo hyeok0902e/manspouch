4ba32e768942b17f3ca3c1805ee41cfa778f6d45
Minseok Bang <minseokbang@Minseokui-MacBookAir.local>
2018-12-27 15:05:25 +0900

    s

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,6 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: [:show, :edit, :update, :destroy, :upvote]
-  before_action :authenticate_user!, only: [:new, :edit, :delete, :upvote, :custom]
+  # before_action :authenticate_user!, only: [:new, :edit, :delete, :upvote, :custom]
   impressionist actions: [:show]
   # load_and_authorize_resource
 
